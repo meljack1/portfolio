5e58b5c9159e0147619a934d1154d03e29c83b2f
Mel Jack <melissa_jack@hotmail.co.uk>
2021-11-11 22:33:06 +0000

    Update information in about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import melPhoto from '../../assets/melphoto.PNG'
 
 export default function About() {
   return (
     <div>
-      <h1 class="uk-heading-line uk-text-center">
-        <span>About</span>
-      </h1>
+        <h1 className="uk-heading-line uk-text-center">
+            <span>About</span>
+        </h1>
+        <div className="circle">
+            <img src={melPhoto} alt="A photograph of Mel Jack"/>
+        </div>
+        <p>
+            I am a Birmingham-based web developer with a strong collaborative work ethic and past experience as a secondary school teacher. My first degree in Chemistry, and I recently earned a certificate in Web Development from the University of Birmingham’s Full-Stack Bootcamp.
+        </p>
+        <p>
+            I am adaptable, a quick learner and devoted problem solver. I enjoy learning new technologies and using them to build my own projects. My experience in teaching has made me an excellent communicator, both verbally and in writing, including keeping my code clean and descriptive.
+        </p>
+        <p>
+            I love programming because I enjoy solving problems, picking things apart and understanding how they work. Being a strong team player, problem solver and fast learner sets me up well as a programmer, and I hope to learn a lot more as my career progresses.
+        </p>
     </div>
   );
 }
