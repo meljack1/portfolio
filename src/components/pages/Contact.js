bc7e63c730c7e422641d230c86402ba450cda0a6
Mel Jack <melissa_jack@hotmail.co.uk>
2021-11-27 03:11:59 +0000

    Add emailJS to test

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('default_service', 'Portfolio', form.current, 'user_tAQWTFl4f5udfilPwKn1L')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <div>
       <h1 className="uk-heading-line uk-text-center">
             <span>Contact</span>
       </h1>
-      <form className="uk-container uk-container-xsmall">
+      <form ref={form} onSubmit={sendEmail}>
         <fieldset className="uk-fieldset">
             <div className="uk-margin">
                 <input className="uk-input" type="text" id="contact-name" placeholder="Name" />
