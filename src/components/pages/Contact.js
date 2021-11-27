fe4b0390141710d902fdd57941384d49bc3324e0
Mel Jack <melissa_jack@hotmail.co.uk>
2021-11-27 03:18:09 +0000

    Tweak submit button and make form items required!

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -23,13 +23,14 @@ export default function Contact() {
       <form ref={form} onSubmit={sendEmail} className="uk-container uk-container-xsmall">
         <fieldset className="uk-fieldset">
             <div className="uk-margin">
-                <input className="uk-input" type="text" id="contact-name" placeholder="Name" />
+                <input className="uk-input" type="text" id="contact-name" placeholder="Name" required />
             </div>
             <div className="uk-margin">
-                <input class="uk-input" type="text" id="contact-email" placeholder="Email" />
+                <input class="uk-input" type="text" id="contact-email" placeholder="Email" required />
             </div>
             <div className="uk-margin">
-                <textarea class="uk-textarea" rows="5" id="contact-message" placeholder="Write your message here"></textarea>
+                <textarea class="uk-textarea" rows="5" id="contact-message" placeholder="Write your message here" required>
+                </textarea>
             </div>
         </fieldset>
         <button className="uk-button uk-button-primary" id="submit-button" type="submit">Submit</button>
