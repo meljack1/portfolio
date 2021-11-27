f8b2eb29f45ba8fd1f01ea5f390bafe61f9c0dcd
Mel Jack <melissa_jack@hotmail.co.uk>
2021-11-27 01:05:42 +0000

    Add form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,9 +3,23 @@ import React from 'react';
 export default function Contact() {
   return (
     <div>
-      <h1 class="uk-heading-line uk-text-center">
-        <span>Contact</span>
+      <h1 className="uk-heading-line uk-text-center">
+            <span>Contact</span>
       </h1>
+      <form>
+        <fieldset className="uk-fieldset">
+            <div className="uk-margin">
+                <input className="uk-input" type="text" id="contact-name" placeholder="Name" />
+            </div>
+            <div className="uk-margin">
+                <input class="uk-input" type="text" id="contact-email" placeholder="Email" />
+            </div>
+            <div className="uk-margin">
+                <textarea class="uk-textarea" rows="5" id="contact-message" placeholder="Write your message here"></textarea>
+            </div>
+        </fieldset>
+        <button className="uk-button uk-button-primary" id="submit-button" type="submit">Submit</button>
+      </form>
     </div>
   );
 }
