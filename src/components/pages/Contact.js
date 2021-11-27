10ef8efa592c7587213090e12036e468d57f64a0
Mel Jack <melissa_jack@hotmail.co.uk>
2021-11-27 04:21:53 +0000

    Ensure email.js functions properly

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,15 +1,28 @@
-import React, {useRef} from 'react';
+import React, {useState} from 'react';
 import emailjs from 'emailjs-com';
 import UIkit from 'uikit';
 export default function Contact() {
 
-  const form = useRef();
+  const [input, setValues] = useState({
+    name: "",
+    email: "",
+    message: ""
+  }); 
+
+  const handleInputChange = (prop) => (event) => {
+    setValues({ ...input, [prop]: event.target.value });
+    console.log(input);
+  };
+
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('default_service', 'Portfolio', form.current, 'user_tAQWTFl4f5udfilPwKn1L')
+    emailjs.send('service_bsk2mm8', "template_knz0757", {
+      name: input.name,
+      email: input.email,
+      message: input.message
+    }, 'user_tAQWTFl4f5udfilPwKn1L')
       .then((result) => {
-          console.log(result.text);
+          window.location.reload();
       }, (error) => {
           UIkit.modal.alert('There was an error in submitting the form. Try again later, or contact me directly at mel.jack.developer@gmail.com')
           console.log(error.text);
@@ -21,16 +34,16 @@ export default function Contact() {
       <h1 className="uk-heading-line uk-text-center">
             <span>Contact</span>
       </h1>
-      <form ref={form} onSubmit={sendEmail} className="uk-container uk-container-xsmall">
+      <form onSubmit={sendEmail} className="uk-container uk-container-xsmall">
         <fieldset className="uk-fieldset">
             <div className="uk-margin">
-                <input className="uk-input" type="text" id="contact-name" placeholder="Name" required />
+                <input className="uk-input" type="text" id="contact-name" placeholder="Name" onChange={handleInputChange("name")} required />
             </div>
             <div className="uk-margin">
-                <input class="uk-input" type="text" id="contact-email" placeholder="Email" required />
+                <input class="uk-input" type="text" id="contact-email" placeholder="Email" onChange={handleInputChange("email")} required />
             </div>
             <div className="uk-margin">
-                <textarea class="uk-textarea" rows="5" id="contact-message" placeholder="Write your message here" required>
+                <textarea class="uk-textarea" rows="5" id="contact-message" placeholder="Write your message here" onChange={handleInputChange("message")} required>
                 </textarea>
             </div>
         </fieldset>
